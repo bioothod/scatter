ec87c15d34820db8431ae4b4dda9202d5e883754
Evgeniy Polyakov <zbr@ioremap.net>
2016-04-09 02:32:24 +0300

    route: print route table updates into log

--- src/route.cpp
+++ src/route.cpp
@@ -1,23 +1,38 @@
 #include "scatter/route.hpp"
 
+#include <glog/logging.h>
+
 namespace ioremap { namespace scatter {
 
 void route::add(connection::pointer cn)
 {
 	std::vector<connection::cid_t> cids = cn->ids();
+	std::ostringstream ss;
+
 	std::lock_guard<std::mutex> guard(m_lock);
 	for (auto &id: cids) {
 		m_connections[id] = cn;
+		ss << id;
+		if (id != cids.back())
+			ss << ",";
 	}
+
+	LOG(INFO) << "connection: " << cn->connection_string() << ", added routes: " << ss.str();
 }
 
 void route::remove(connection::pointer cn)
 {
 	std::vector<connection::cid_t> cids = cn->ids();
+	std::ostringstream ss;
+
 	std::lock_guard<std::mutex> guard(m_lock);
 	for (auto &id: cids) {
 		m_connections.erase(id);
+		ss << id;
+		if (id != cids.back())
+			ss << ",";
 	}
+	LOG(INFO) << "connection: " << cn->connection_string() << ", removed routes: " << ss.str();
 }
 
 connection::pointer route::find(uint64_t id)
