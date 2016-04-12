961e07c49d0c016f6e694f15795e7a585d7dad2e
Evgeniy Polyakov <zbr@ioremap.net>
2016-04-12 13:20:12 +0300

    test: added one-to-one bidirectional test over different servers: c1-s1-s2-c2

--- test/main.cpp
+++ test/main.cpp
@@ -480,6 +480,93 @@ TEST_F(stest, leave_broadcast_groups)
 	ASSERT_EQ(m_s2.test_bcast_num_connections(db, true), 0);
 }
 
+TEST_F(stest, bcast_one_to_one_bidirectional_different_servers)
+{
+	std::vector<connection::cid_t> ids1 = {0, 500};
+	std::vector<connection::cid_t> ids2 = {100, 600};
+
+	set_ids(ids1, ids2);
+	connect_servers();
+
+	node c1, c2, c3;
+	uint64_t db = 123;
+
+	int c1_bias = 10000;
+	int c2_bias = 30000;
+
+
+	std::mutex lock;
+	std::condition_variable cv1, cv2;
+
+	std::atomic_int c1_counter(0), c1_completed(0);
+	c1.connect(m_addr1, [&] (connection::pointer cn, message &msg) {
+					VLOG(2) << "connection: " << cn->connection_string() <<
+						", client1 received message: " << msg.to_string();
+					ASSERT_EQ(msg.id(), c2_bias + c1_counter);
+					c1_counter++;
+			});
+	c1.bcast_join(db);
+
+	std::atomic_int c2_counter(0), c2_completed(0);
+	c2.connect(m_addr2, [&] (connection::pointer cn, message &msg) {
+					VLOG(2) << "connection: " << cn->connection_string() <<
+						", client2 received message: " << msg.to_string();
+					ASSERT_EQ(msg.id(), c1_bias + c2_counter);
+					c2_counter++;
+			});
+	c2.bcast_join(db);
+
+	int n = 10000;
+	for (int i = 0; i < n; ++i) {
+		message msg;
+		msg.hdr.id = c1_bias + i;
+		msg.hdr.db = db;
+		msg.hdr.cmd = SCATTER_CMD_CLIENT + 1;
+		msg.hdr.flags = SCATTER_FLAGS_NEED_ACK;
+		msg.encode_header();
+
+		c1.send(msg, [&] (connection::pointer, message &reply) {
+					ASSERT_EQ(reply.id(), c1_bias + c1_completed.load());
+					c1_completed++;
+					cv1.notify_one();
+				});
+		message m2;
+		m2.hdr.id = c2_bias + i;
+		m2.hdr.db = db;
+		m2.hdr.cmd = SCATTER_CMD_CLIENT + 1;
+		m2.hdr.flags = SCATTER_FLAGS_NEED_ACK;
+		m2.encode_header();
+		c2.send(m2, [&] (connection::pointer, message &reply) {
+					ASSERT_EQ(reply.id(), c2_bias + c2_completed.load());
+					c2_completed++;
+					cv2.notify_one();
+				});
+	}
+
+	VLOG(2) << "going to wait" <<
+		": c1_completed: " << c1_completed <<
+		", c1_counter: " << c1_counter <<
+		": c2_completed: " << c2_completed <<
+		", c2_counter: " << c2_counter <<
+		", n: " << n;
+
+	std::unique_lock<std::mutex> l1(lock);
+	cv1.wait_for(l1, std::chrono::seconds(1000), [&] {return c1_completed == n;});
+	l1.unlock();
+
+	ASSERT_EQ(c1_completed, n);
+	ASSERT_EQ(c1_completed, c2_counter);
+
+	std::unique_lock<std::mutex> l2(lock);
+	cv2.wait_for(l2, std::chrono::seconds(1000), [&] {return c2_completed == n;});
+	l2.unlock();
+
+	ASSERT_EQ(c2_completed, n);
+	ASSERT_EQ(c2_completed, c1_counter);
+
+	return;
+}
+
 int main(int argc, char **argv)
 {
 	google::InitGoogleLogging(argv[0]);
