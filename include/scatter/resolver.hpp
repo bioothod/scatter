28efd06406ca0a92da08af3f0f8f61ab63acdae1
Evgeniy Polyakov <zbr@ioremap.net>
2016-04-17 14:54:28 +0300

    resolver: indent cleanup

--- include/scatter/resolver.hpp
+++ include/scatter/resolver.hpp
@@ -20,37 +20,37 @@ public:
 	resolver(io_service_pool &io) : m_pool(io), m_resolver(io.get_service()) {}
 
 	std::future<IterType> resolve(const std::string &addr_str) {
-			std::string::size_type pos_end;
-			pos_end = addr_str.rfind(":");
-			if (pos_end == std::string::npos) {
-				std::promise<IterType> p;
-				std::future<IterType> f = p.get_future();
+		std::string::size_type pos_end;
+		pos_end = addr_str.rfind(":");
+		if (pos_end == std::string::npos) {
+			std::promise<IterType> p;
+			std::future<IterType> f = p.get_future();
 
-				p.set_exception(std::make_exception_ptr(
-						create_error(-EINVAL, "invalid address string '%s', there is no ':' delimiter",
-							addr_str.c_str())));
-				return f;
-			}
+			p.set_exception(std::make_exception_ptr(
+					create_error(-EINVAL, "invalid address string '%s', there is no ':' delimiter",
+						addr_str.c_str())));
+			return f;
+		}
 
-			int family = atoi(addr_str.substr(pos_end + 1).c_str());
+		int family = atoi(addr_str.substr(pos_end + 1).c_str());
 
-			size_t pos_start;
-			pos_end--;
-			pos_start = addr_str.rfind(":", pos_end);
-			if (pos_end == std::string::npos) {
-				std::promise<IterType> p;
-				std::future<IterType> f = p.get_future();
+		size_t pos_start;
+		pos_end--;
+		pos_start = addr_str.rfind(":", pos_end);
+		if (pos_end == std::string::npos) {
+			std::promise<IterType> p;
+			std::future<IterType> f = p.get_future();
 
-				p.set_exception(std::make_exception_ptr(
-						create_error(-EINVAL, "invalid address string '%s', there is no ':' delimiter",
-							addr_str.c_str())));
-				return f;
-			}
+			p.set_exception(std::make_exception_ptr(
+					create_error(-EINVAL, "invalid address string '%s', there is no ':' delimiter",
+						addr_str.c_str())));
+			return f;
+		}
 
-			std::string port = addr_str.substr(pos_start + 1, pos_end - pos_start);
-			std::string addr = addr_str.substr(0, pos_start);
+		std::string port = addr_str.substr(pos_start + 1, pos_end - pos_start);
+		std::string addr = addr_str.substr(0, pos_start);
 
-			return resolve(addr, port, family);
+		return resolve(addr, port, family);
 	}
 
 	std::future<IterType> resolve(const std::string &addr_str, const std::string &port_str, int family = AF_INET) {
@@ -68,7 +68,7 @@ public:
 			return f;
 		}
 
-		boost::asio::ip::tcp::resolver::query q{proto, addr_str, port_str};
+		typename ProtoType::resolver::query q{proto, addr_str, port_str};
 		return m_resolver.async_resolve(q, boost::asio::use_future);
 	}
 
